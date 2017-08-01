6f690bb336bbae63b9382fd94d31165225b1b42b
Dragon1207 <smileboth0805@outlook.com>
2017-08-01 23:50:54 +0300

    Change `add` to `create` in routing

--- src/music/urls.py
+++ src/music/urls.py
@@ -6,7 +6,7 @@ from . import views
 urlpatterns = [
     url(r'^$', views.AlbumListView.as_view(), name='album_list'),
     url(r'^(?P<pk>\d+)$', views.AlbumDetailView.as_view(), name='album_detail'),
-    url(r'^album/add/$', views.AlbumCreateView.as_view(), name='album_create'),
+    url(r'^album/create/$', views.AlbumCreateView.as_view(), name='album_create'),
     url(r'^album/(?P<pk>\d+)/$', views.AlbumUpdateView.as_view(), name='album_update'),
     url(r'^album/(?P<pk>\d+)/delete/$', views.AlbumDeleteView.as_view(), name='album_delete'),
 ]
