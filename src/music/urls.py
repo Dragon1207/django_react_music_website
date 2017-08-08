7b470c6e59228f80fcf6134de992d83c17e09e5f
Dragon1207 <smileboth0805@outlook.com>
2017-08-08 12:41:49 +0300

    Move page from URL to URL parameter

--- src/music/urls.py
+++ src/music/urls.py
@@ -5,7 +5,7 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns = [
-    url(r'^(?:page/(?P<page>\d+))?$', views.AlbumListView.as_view(), name='album_list'),
+    url(r'^$', views.AlbumListView.as_view(), name='album_list'),
     url(r'^(?P<pk>\d+)$', views.AlbumDetailView.as_view(), name='album_detail'),
     url(r'^album/create/$', views.AlbumCreateView.as_view(), name='album_create'),
     url(r'^album/(?P<pk>\d+)/$', views.AlbumUpdateView.as_view(), name='album_update'),
