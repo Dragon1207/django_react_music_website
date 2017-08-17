4e28c250e659ec1264adb388e27b0f34d8ec131a
Dragon1207 <smileboth0805@outlook.com>
2017-08-17 22:59:33 +0300

    Add unit tests

--- src/music/tests.py
+++ src/music/tests.py
@@ -1,3 +1,14 @@
 from django.test import TestCase
+from music.models import Album, Song
 
-# Create your tests here.
+
+class AlbumTests(TestCase):
+    def test_str(self):
+        p = Album(artist='MyArtist', album_title='MyAlbumTitle', genre='MyGenre')
+        self.assertEqual(str(p), 'MyAlbumTitle . MyArtist')
+
+
+class SongTests(TestCase):
+    def test_str(self):
+        s = Song(file_type='MyFileType', song_title='MySongTitle', is_favorite=False)
+        self.assertEqual(str(s), 'MySongTitle')
