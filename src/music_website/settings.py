6976b85252d190a33de64b8d17948ffad6cdf07e
Dragon1207 <smileboth0805@outlook.com>
2017-11-04 17:15:12 +0300

    Update packages

--- src/music_website/settings.py
+++ src/music_website/settings.py
@@ -233,5 +233,6 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
         'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
     ],
-    'PAGE_SIZE': 10
+    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination'
 }
