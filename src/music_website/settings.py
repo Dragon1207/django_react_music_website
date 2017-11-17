4127f822dff64a0162d9f5adb7eeb9da6967dae7
Dragon1207 <smileboth0805@outlook.com>
2017-11-17 23:30:40 +0300

    Fix isort warning

--- src/music_website/settings.py
+++ src/music_website/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/1.11/ref/settings/
 import os
 
 import dj_database_url
-from decouple import config, Csv
+from decouple import Csv, config
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
