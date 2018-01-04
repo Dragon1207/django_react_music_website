b7c7ab31c6c12a5133e7f0247a438c42de83aa6c
Dragon1207 <smileboth0805@outlook.com>
2018-01-04 14:50:48 +0300

    Separate vendor dependencies in Webpack

--- webpack.config.js
+++ webpack.config.js
@@ -11,11 +11,13 @@ const rootAssetPath = path.join(__dirname, "static");
         return {
             entry: {
                 main: [
+                    path.join(rootAssetPath, "css", "main.css"),
+                    path.join(rootAssetPath, "img", "background.png")
+                ],
+                vendor: [
                     "jquery", // jQuery is required by taggit-selectize
                     "bootstrap",
                     "bootstrap/dist/css/bootstrap.css",
-                    path.join(rootAssetPath, "css", "main.css"),
-                    path.join(rootAssetPath, "img", "background.png")
                 ],
                 "selectize": [path.join(rootAssetPath, "css", "taggit_selectize", "css", "selectize.bootstrap3.css")]
             },
