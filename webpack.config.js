d3f301e3dabe963a950f04d462d510008fa4624b
Dragon1207 <smileboth0805@outlook.com>
2018-02-17 21:14:43 +0300

    Fix jshint warnings

--- webpack.config.js
+++ webpack.config.js
@@ -1,53 +1,67 @@
-"use strict";
-const path = require("path");
-const webpack = require("webpack");
-const BundleTracker = require("webpack-bundle-tracker");
-const ExtractTextPlugin = require("extract-text-webpack-plugin");
-const rootAssetPath = path.join(__dirname, "static");
+// jshint esversion: 6
+(function () {
+    "use strict";
+    const ExtractTextPlugin = require("extract-text-webpack-plugin");
+    const webpack = require("webpack");
 
-(function (extractCss, webpack2) {
-    module.exports = (env) => {
-        const isDevBuild = !(env && env.prod);
-        return {
-            entry: {
-                main: [
-                    path.join(rootAssetPath, "css", "main.css"),
-                    path.join(rootAssetPath, "img", "background.png")
-                ],
-                vendor: [
-                    "jquery", // jQuery is required by taggit-selectize
-                    "bootstrap",
-                    "bootstrap/dist/css/bootstrap.css",
-                ],
-                "selectize": [path.join(rootAssetPath, "css", "taggit_selectize", "css", "selectize.bootstrap3.css")]
-            },
-            output: {
-                path: path.join(rootAssetPath, "static"),
-                publicPath: "/static/",
-                filename: "[name].[hash].js",
-                library: "[name]_[hash]"
-            },
-            resolve: {extensions: [".js", ".css"]},
-            module: {
-                rules: [
-                    {
-                        test: /jquery\.(jsx|js)$/,
-                        loader: "expose-loader?jQuery"
-                    },
-                    {
-                        test: /\.css(\?|$)/,
-                        use: extractCss.extract({use: [
-                            isDevBuild ? "css-loader" : "css-loader?minimize", "postcss-loader"
-                        ]})
-                    },
-                    {test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/, use: "url-loader?limit=100000"}
-                ]
-            },
-            stats: {modules: false},
-            plugins: [
-                extractCss,
-                new BundleTracker({filename: path.join("static", "manifest.json")})
-            ].concat(isDevBuild ? [] : [new webpack2.optimize.UglifyJsPlugin()])
+    (function (extractCss, webpack2) {
+        const path = require("path");
+        const BundleTracker = require("webpack-bundle-tracker");
+        const rootAssetPath = path.join(__dirname, "static");
+
+        module.exports = (env) => {
+            const isDevBuild = !(env && env.prod);
+            return {
+                entry: {
+                    main: [
+                        path.join(rootAssetPath, "css", "main.css"),
+                        path.join(rootAssetPath, "img", "background.png")
+                    ],
+                    vendor: [
+                        "jquery", // jQuery is required by taggit-selectize
+                        "bootstrap",
+                        "bootstrap/dist/css/bootstrap.css",
+                    ],
+                    "selectize": [path.join(rootAssetPath, "css", "taggit_selectize", "css", "selectize.bootstrap3.css")]
+                },
+                output: {
+                    path: path.join(rootAssetPath, "static"),
+                    publicPath: "/static/",
+                    filename: "[name].[hash].js",
+                    library: "[name]_[hash]"
+                },
+                resolve: {
+                    extensions: [".js", ".css"]
+                },
+                module: {
+                    rules: [{
+                            test: /jquery\.(jsx|js)$/,
+                            loader: "expose-loader?jQuery"
+                        },
+                        {
+                            test: /\.css(\?|$)/,
+                            use: extractCss.extract({
+                                use: [
+                                    isDevBuild ? "css-loader" : "css-loader?minimize", "postcss-loader"
+                                ]
+                            })
+                        },
+                        {
+                            test: /\.(png|woff|woff2|eot|ttf|svg)(\?|$)/,
+                            use: "url-loader?limit=100000"
+                        }
+                    ]
+                },
+                stats: {
+                    modules: false
+                },
+                plugins: [
+                    extractCss,
+                    new BundleTracker({
+                        filename: path.join("static", "manifest.json")
+                    })
+                ].concat(isDevBuild ? [] : [new webpack2.optimize.UglifyJsPlugin()])
+            };
         };
-    };
-}(new ExtractTextPlugin("[name].[chunkhash].css"), webpack));
+    }(new ExtractTextPlugin("[name].[chunkhash].css"), webpack));
+}());
