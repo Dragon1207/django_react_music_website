b508b17782fd91dc2789023fa0139756f28bcaee
Dragon1207 <smileboth0805@outlook.com>
2018-02-28 13:10:11 +0300

    Fix slug for Admin panel

--- src/album/admin.py
+++ src/album/admin.py
@@ -3,9 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from album.models import Album
 
-
-class AlbumAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title', )}
-
-
-admin.site.register(Album, AlbumAdmin)
+admin.site.register(Album)
--- src/track/admin.py
+++ src/track/admin.py
@@ -3,10 +3,4 @@ from django.contrib import admin
 # Register your models here.
 from track.models import Track
 
-
-class TrackAdmin(admin.ModelAdmin):
-    prepopulated_fields = {'slug': ('title',)}
-    # TODO: Make prepopulated_fields somehow fill `slug` field based on (album.title, title)
-
-
-admin.site.register(Track, TrackAdmin)
+admin.site.register(Track)
